c78e3586435fb49cea240e79122908a7ebcf16d4
Bilal Yasin <byasin@tier2consulting.com>
2021-07-14 15:56:47 +0100

    Completed Authentication and Authorization Section

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {AuthHttp} from 'angular2-jwt';
+
+@Injectable()
+export class OrderService {
+
+  constructor(private http: AuthHttp) {
+  }
+
+  getOrders() {
+    return this.http.get('/api/orders')
+      .map(response => response.json());
+  }
+}
